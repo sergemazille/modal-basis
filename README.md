f5207645156af27c9b9faafacf9d7537abcdd664
Serge Mazille <serge.mazille@gmail.com>
2017-10-22 13:18:48 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Modal Basis
+
+Simple modal component
+
+Works on modern browsers only (uses es6 modules and css custom properties)
---
+++ src/modal.js
@@ -0,0 +1,103 @@
+import Overlay from 'overlay-basis/src/overlay';
+
+export default class {
+    static init() {
+
+        // create modal's overlay
+        this.overlay = new Overlay();
+
+        // register triggering events
+        registerEvents(this);
+    }
+
+    static show(modal) {
+
+        // show overlay if not explicitly forbidden
+        if (! modal.classList.contains('--no-overlay')) {
+
+            // lock overlay if need be
+            if (modal.classList.contains('--lock-overlay')) {
+                this.overlay.lock();
+            }
+
+            this.overlay.show();
+        }
+
+        modal.classList.add('is-visible');
+    }
+
+    static hide(modal) {
+        modal.classList.remove('is-visible');
+
+        // if there are no other modal element on view, the overlay has to be hidden as well
+        if (! document.querySelectorAll('.modal.is-visible').length) {
+            this.overlay.hide();
+        }
+    }
+}
+
+function registerEvents(Modal) {
+
+    // listen to a click event on a trigger element
+    document.querySelector('body').addEventListener('click', function(e) {
+        let clickedElement = e.target;
+
+        // opening the 'modal' element
+        // ===========================
+
+        // clicked element has to target explicitly a 'modal' element
+        if (clickedElement.dataset.toggle === 'modal') {
+
+            let modal;
+
+            // targeted 'modal' element is passed via data-target attribute
+            if (clickedElement.dataset.target) {
+                let modalSelector = clickedElement.dataset.target;
+                modal = document.querySelector(modalSelector);
+            }
+
+            // targeted 'modal' element is passed via href attribute
+            else if (clickedElement.getAttribute('href')) {
+                let modalSelector = clickedElement.getAttribute('href');
+                modal = document.querySelector(modalSelector);
+            }
+
+            // targeted 'modal' element belongs to the same '.modal-group' than trigger
+            else {
+                let modalGroup = clickedElement.closest('.modal-group');
+
+                if (modalGroup) {
+                    modal = modalGroup.querySelector('.modal');
+                }
+            }
+
+            if (modal) {
+                Modal.show(modal);
+            }
+        }
+
+        // closing the 'modal' element
+        // ===========================
+
+        // dismiss button has to be nested inside the 'modal' element
+        if (clickedElement.classList.contains('dismiss') && clickedElement.closest('.modal')) {
+            let modal = clickedElement.closest('.modal');
+            Modal.hide(modal);
+        }
+
+        // page's overlay was clicked
+        if (clickedElement.id === 'overlay') {
+            let overlay = clickedElement; // for ease of use
+
+            // do nothing if overlay isn't visible or is locked
+            if (! (overlay.classList.contains('is-visible')) || (overlay.classList.contains('is-locked'))) {
+                return;
+            }
+
+            let modals = document.querySelectorAll('.modal');
+            [...modals].forEach(function(modal) {
+                Modal.hide(modal);
+            });
+        }
+    });
+}
